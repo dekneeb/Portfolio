83f4b85ed197710bf6b98012390b4496c2c7c55d
Denee Boyd <deneeoliviaboyd@gmail.com>
2022-04-07 00:30:14 -0500

    deploying to netlify

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id='home'>
         <Nav/>
         <h3 className='header'> Hello World, my name is</h3>
      <h1 className='firstname'> Denee Boyd </h1>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,14 +5,20 @@ import { Link as ScrollLink } from 'react-scroll'
 const Nav=()=>{
     return(
         <div className="nav">
+
+            <ScrollLink to='home' spy={true} smooth={true} offset={-50} duration={400} >
+                        <h3 className='nav-about px-2'>Home</h3>
+                    </ScrollLink> 
            
             <ScrollLink to='about' spy={true} smooth={true} offset={-50} duration={400} >
                         <h3 className='nav-about px-2'>About</h3>
                     </ScrollLink> 
                     
-          <ScrollLink to='projects' spy={true} smooth={true} offset={-50} duration={400} >
+            <ScrollLink to='projects' spy={true} smooth={true} offset={-50} duration={400} >
                         <h3 className='nav-about px-2'>Projects</h3>
                     </ScrollLink>
+
+            <a  href={require('../images/resume.pdf')}><h3 className='nav-about px-2'>Resume</h3></a>
                     
            
         </div>
