c2f96fe14576adb845412003b7dcfef46e399f10
Denee Boyd <deneeoliviaboyd@gmail.com>
2022-04-07 20:06:56 -0500

    project thumbnails improved

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,7 +26,7 @@ const Project = () =>{
                 <a href='https://friendly-babbage-f70de5.netlify.app/'><img className='photo projectsicon' src="brewtour.jpg"></img>
                 <div className='overlay'>
                     <div className='description'>Tech Stack: React, Brewery API </div>
-                    <div className='words'>A search engine for nearby Breweries. Users can favorite certain breweries and keep a running list of ones already visited, as well as directly access websites and get directions.</div>
+                    <div className='words'>A search engine for nearby Breweries. Users can favorite certain breweries and keep a running list of ones already visited, as well as directly access websites and get directions. Individual project week 9.</div>
                 </div>
                 </a>
             </div>
@@ -34,13 +34,12 @@ const Project = () =>{
                 <a href='https://i-do-two.herokuapp.com/'><img className='photo projectsicon' src="Idotwo.png"></img>
                 <div className='overlay'>
                     <div className='description'>Tech Stack: Django, PostreSQL, Python, AWS, Bootstrap</div>
-                    <div className='words'>Niche resale website for weddings. Users can comment and direct/private message for exchange of items.</div>
+                    <div className='words'>Niche resale website for weddings. Users can comment and direct/private message for exchange of items. Individual project week 12.</div>
                     <span><a href='https://github.com/dekneeb/IDoTwo'><FiIcons.FiGithub/></a>
                     <a href='https://i-do-two.herokuapp.com/'><BsIcons.BsGlobe/></a></span>
-                    
-
                 </div>
                 </a>
+                
             </div>
             </div>
         </div>
