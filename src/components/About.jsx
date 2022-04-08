5237507cc316d65226d5c9ad4d66e350a1663ffe
Denee Boyd <deneeoliviaboyd@gmail.com>
2022-04-08 13:45:14 -0500

    button color changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,17 @@ const About = () =>{
             <img className='headshot' src="headshot.jpeg"></img>
             </div>
             <div className='second-column'>
-            <p className='paragraph'> Hello! My name is Denee (Den-ay), I'm glad you've found my portfolio. The genesis of my web development interests could be accredited to 2006 coding html for my MySpace profile, however more accurately I recently completed an Immersive Software Engineering course with General Assembly.  Presently I'm seeking a work place that will challenge my current skill set, and foster an environment of learning and growth. I gravitate towards backend engineering, but am capable of full stack development. </p>
+            <p className='paragraph'> Hello! My name is Denee (Den-ay), I'm glad you've found my portfolio. The genesis of my web development interests came about during the pandemic when I did an introspective look and complete career overhaul. I recently completed an Immersive Software Engineering course with General Assembly.  Presently I'm seeking a work place that will challenge my current skill set, and foster an environment of learning and growth. I gravitate towards backend engineering, but am capable of full stack development. 
+            
+            Hello! My name is Denee (Den-ay), I'm glad you've found my portfolio. Let me tell you a bit about myself.
+ 
+I’ve searched high and low for a career I’m passionate about. During my professional career I have worked in both the healthcare and real-estate fields and while I learned a lot from both, it was not until recently that I truly discovered the career I was meant to pursue.
+ 
+Coding has completely changed the vision I have for my life. Having recently completed General Assembly’s 500+ hour Immersive Software Engineering course, I’m seeking work where I can flex this new-found coding muscle. I want to be helping to produce high-quality, optimized code in an environment which supports learning, growth, and innovation.
+
+Outside of work, I’m always striving to shape myself into the best version of me. This means always prioritizing a healthy body and mind so that I can be at my best when nurturing important aspects of my life like work and my relationships. I have carried this mentality into any job I have worked and have been an asset in all positions I’ve held as a result. Now, as I embark on my career in tech, I hope to find work with an organization which values its people, invests in their futures, and builds a bad-ass product.
+
+I’d love to talk with you if you are interested.</p>
             <p className='paragraph'>A few languages and frameworks I've worked with recently:
                 <ul>
                     <li><img className='icon'src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
